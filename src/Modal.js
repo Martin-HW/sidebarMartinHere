19552d7ae18e1076702c87e86978e679c1d74fb1
martin_suarez <martin_suarez_mendez@outlook.com>
2023-01-09 13:26:22 -0600

    app done

---
+++ src/Modal.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { useGlobalContext } from "./context";
+import { FaTimes } from "react-icons/fa";
+import logo from "./globantmex.jpg";
+const Modal = () => {
+  const { isModalOpen, closeModal } = useGlobalContext();
+  return (
+    <div
+      className={`${
+        isModalOpen ? "modal-overlay show-modal" : "modal-overlay"
+      }`}
+    >
+      <div className="modal-container">
+        <h3>Martin's modal content</h3>
+        <p>
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book. It has survived not only
+          five centuries, but also the leap into electronic typesetting,
+          remaining essentially unchanged. It was popularised in the 1960s with
+          the release of Letraset sheets containing Lorem Ipsum passages, and
+          more recently with desktop publishing software like Aldus PageMaker
+          including versions of Lorem Ipsum.
+        </p>
+        <img className="globantImg" src={logo} alt={logo} />
+
+        <button className="close-modal-btn" onClick={closeModal}>
+          <FaTimes></FaTimes>
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
